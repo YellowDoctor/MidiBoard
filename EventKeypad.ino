8bf81064c77f754b6cde1ea92874872b282f1878
YellowDoctor <jenuataran55@gmail.com>
2018-03-09 21:56:02 +0200

    q

--- EventKeypad.ino
+++ EventKeypad.ino
@@ -14,12 +14,12 @@ char keys[ROWS][COLS] = {
   {'7', '8', '9'},
   {'*', '0', '#'}
 };
-volatile int Notes[] = {72,74,76,77,79,81,83,84,86,};
+volatile int Notes[] = {72,74,76,77,79,81,83,84,86,};//Notes
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3};
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, HairlessMidiSettings);
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-volatile unsigned int Chanel = 1; 
+volatile unsigned int Channel = 1; 
 volatile bool SB = 0;
 
 
@@ -61,15 +61,15 @@ void keypadEvent(KeypadEvent key) {
       NoteNumber = 8;
       break;
     case '*':
-      Chanel++;
+      Channel++;
       SB = 1;
       break;
     case '0':
       SB = 1;
-      Chanel = 1
+      Chanel = 1; //Set Channel to one
       break;
     case '#':
-      Chanel--;
+      Channel--;
       SB = 1;
       break;
   }
@@ -79,11 +79,11 @@ SB = 0;
   else{
   switch (keypad.getState()) {
     case PRESSED:
-      MIDI.sendNoteOn(Notes[NoteNumber], 127, Chanel);
+      MIDI.sendNoteOn(Notes[NoteNumber], 127, Channel);
       break;
 
     case RELEASED:
-      MIDI.sendNoteOff(Notes[NoteNumber], 127, Chanel);
+      MIDI.sendNoteOff(Notes[NoteNumber], 127, Channel);
       break;
   }
   }
