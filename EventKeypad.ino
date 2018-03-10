d9830fd3aeb8f98203e1993acdd11310f617e0ed
YellowDoctor <jenuataran55@gmail.com>
2018-03-10 23:02:21 +0200

    Led support

--- EventKeypad.ino
+++ EventKeypad.ino
@@ -21,8 +21,8 @@ MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, HairlessMidiSettings);
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 volatile unsigned int Channel = 1; 
 volatile bool SB = 0;
-volatile int LedPin[] = {}//LedPin
-
+volatile int LedPin[] = {D2,D10,D11,D12,A0,A1,A2,A3};//LedPin
+volatile int LedNumber;
 void setup() {
   Serial.begin(115200);
   keypad.addEventListener(keypadEvent);
@@ -38,27 +38,35 @@ void keypadEvent(KeypadEvent key) {
   switch (key) {
     case '1':
       NoteNumber = 0;
+      LedNumber = 0;
       break;
     case '2':
       NoteNumber = 1;
+      LedNumber = 1;
       break;
     case '3':
       NoteNumber = 2;
+      LedNumber = 2;
       break;
     case '4':
       NoteNumber = 3;
+      LedNumber = 3;
       break;
     case '5':
       NoteNumber = 4;
+      LedNumber = 4;
       break;
     case '6':
       NoteNumber = 5;
+      LedNumber = 5;
       break;
     case '7':
       NoteNumber = 6;
+      LedNumber = 6;
       break;
     case '8':
       NoteNumber = 7;
+      LedNumber = 7;
       break;
     case '9':
       NoteNumber = 8;
@@ -83,10 +91,12 @@ SB = 0;
   switch (keypad.getState()) {
     case PRESSED:
       MIDI.sendNoteOn(Notes[NoteNumber], 127, Channel);
+      digitalWrite(LedPin[LedNumber],HIGH);
       break;
 
     case RELEASED:
       MIDI.sendNoteOff(Notes[NoteNumber], 127, Channel);
+      digitalWrite(LedPin[LedNumber],LOW);
       break;
   }
   }
