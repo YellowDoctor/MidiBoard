9447b7b9ef2217da6a67452754b2ffeab47c4e42
YellowDoctor <37218143+YellowDoctor@users.noreply.github.com>
2018-03-16 17:46:59 +0200

    Add files via upload

--- EventKeypad.ino
+++ EventKeypad.ino
@@ -17,35 +17,36 @@ char keys[ROWS][COLS] = {
 volatile int Octave;
 volatile int i;
 volatile int Notes[10] [7] = {
-  {0,2,4,5,7,9,11},
-  {12,14,16,17,19,21,23},
-  {24,26,28,29,32,33,35},
-  {36,38,40,41,43,45,47},
-  {48,50,52,53,55,57,59},
-  {60,62,64,65,67,69,71},
-  {72,74,76,77,79,81,83},
-  {84,86,88,89,91,93,95},
-  {96,98,100,101,103,105,107},
-  {108,110,112,113,115,117,119}
-  };//Notes
+  {0, 2, 4, 5, 7, 9, 11},
+  {12, 14, 16, 17, 19, 21, 23},
+  {24, 26, 28, 29, 32, 33, 35},
+  {36, 38, 40, 41, 43, 45, 47},
+  {48, 50, 52, 53, 55, 57, 59},
+  {60, 62, 64, 65, 67, 69, 71},
+  {72, 74, 76, 77, 79, 81, 83},
+  {84, 86, 88, 89, 91, 93, 95},
+  {96, 98, 100, 101, 103, 105, 107},
+  {108, 110, 112, 113, 115, 117, 119}
+};//Notes
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3};
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, HairlessMidiSettings);
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-volatile unsigned int Channel = 1; 
+volatile unsigned int Velocity = 127;
 volatile bool SB = 0;
-volatile int LedPin[] = {2,10,11,12,A0,A1,A2,A3};//LedPin
+volatile int LedPin[] = {2, 10, 11, 12, A0, A1, A2, A3}; //LedPin
 volatile int LedNumber;
+volatile int Buzzer;
 void setup() {
   Serial.begin(115200);
   keypad.addEventListener(keypadEvent);
-  for (i=0;i!=8;i++){
+  for (i = 0; i != 8; i++) {
     pinMode(LedPin[i], OUTPUT);
   }
 }
 
 void loop() {
-char key = keypad.getKey();
+  char key = keypad.getKey();
 }
 void keypadEvent(KeypadEvent key) {
   switch (key) {
@@ -78,69 +79,76 @@ void keypadEvent(KeypadEvent key) {
       LedNumber = 6;
       break;
     case '8':
-    
+      Velocity-=6;
+      SB = 1;
       break;
     case '9':
-      
+      Velocity+=6;
+      SB = 1;
       break;
     case '*':
-      Channel++;
+      Velocity = 1;
       SB = 1;
       break;
     case '0':
       SB = 1;
-      Channel = 1; //Set Channel to one
+      Velocity = 63;
       break;
     case '#':
-      Channel--;
+      Velocity = 127;
       SB = 1;
       break;
   }
- volatile int Pot = analogRead(A4);
-  if(Pot < 102.3){
+  volatile int Pot = analogRead(A4);
+  if (Pot < 102.3) {
     Octave = 0;
   }
-  else if(Pot < 204.6){
+  else if (Pot < 204.6) {
     Octave = 1;
   }
-  else if(Pot < 306.9){
+  else if (Pot < 306.9) {
     Octave = 2;
   }
-  else if(Pot < 409.2){
+  else if (Pot < 409.2) {
     Octave = 3;
   }
-  else if(Pot < 511.5){
+  else if (Pot < 511.5) {
     Octave = 4;
   }
-  else if(Pot < 613.8){
+  else if (Pot < 613.8) {
     Octave = 5;
   }
-  else if(Pot < 716.1){
+  else if (Pot < 716.1) {
     Octave = 6;
   }
-  else if(Pot < 818.4){
+  else if (Pot < 818.4) {
     Octave = 7;
   }
-  else if(Pot < 920.7){
+  else if (Pot < 920.7) {
     Octave = 8;
   }
-  else if(Pot < 1023){
+  else if (Pot < 1023) {
     Octave = 9;
   }
-  if (SB){
-SB = 0;
-  }
-  else{
-  switch (keypad.getState()) {
-    case PRESSED:
-      MIDI.sendNoteOn(Notes [Octave] [NoteNumber], 127, Channel);
-      digitalWrite(LedPin[LedNumber],HIGH);
-      break;
+  if (SB) {
+    if (Velocity>=128){
+      Velocity = 127;
+      }
+    SB = 0;
 
-    case RELEASED:
-      MIDI.sendNoteOff(Notes [Octave] [NoteNumber], 127, Channel);
-      digitalWrite(LedPin[LedNumber],LOW);
-      break;
   }
+  else {
+    switch (keypad.getState()) {
+      case PRESSED:
+        MIDI.sendNoteOn(Notes [Octave] [NoteNumber], Velocity, 1);
+        digitalWrite(LedPin[LedNumber], HIGH);
+        //Serial.println(Notes [Octave] [NoteNumber]);
+        break;
+
+      case RELEASED:
+        MIDI.sendNoteOff(Notes [Octave] [NoteNumber], Velocity, 1);
+        digitalWrite(LedPin[LedNumber], LOW);
+        break;
+    }
   }
 }
